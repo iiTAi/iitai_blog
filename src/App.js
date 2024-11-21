7af85fcc77603003c68f2f96e253e5ab31c30691
iiTAi <kimagureowata@icloud.com>
2024-11-21 11:14:47 +0900

    commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <meta charset="UTF-8" lang="ja" />
+        <title>iiTAiの部屋</title>
       </header>
+      
+      <body>
+        <h1>
+          iiTAiの部屋
+        </h1>
+      </body>
     </div>
   );
 }
