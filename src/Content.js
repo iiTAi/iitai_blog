f024e3696cb22464f5324b86271048e52cb982b8
iiTAi <kimagureowata@icloud.com>
2024-11-21 18:36:29 +0900

    記事リストの追加

--- src/Content.js
+++ src/Content.js
@@ -4,7 +4,6 @@ import './Content.css'
 function Content() {
     return (
         <div className='content'>
-            {/* // 中央ぞろえのgrid2 */}
             <Grid2 container justifyContent={'center'} alignItems={'center'} sx={{paddingX: '8.0%'}}>
                 <Grid2 lg={8} sm={8} spacing={10}>
                     {/* <p style={{ textAlign: 'center' }}> */}
@@ -13,6 +12,38 @@ function Content() {
                         ここはわたしの心底どうでもいいことを書いていくブログです。<br/><br/>
                         Reactの勉強がてら作っているサイトなので、クオリティの低さはお許しを。
                     </p>
+                </Grid2>
+            </Grid2>
+            <Grid2 container direction={'row'} spacing={2}>
+            <Grid2 size={6}>
+                    <h2 style={{ textAlign: 'center' }}>
+                        Markdown
+                    </h2>
+                </Grid2>
+                <Grid2 size={6}>
+                    <h2 style={{ textAlign: 'center' }}>
+                        Others
+                    </h2>
+                </Grid2>
+                <Grid2
+                size={6}
+                sx={{
+                    backgroundColor: '#ffffff20',
+                    padding: '1em',
+                    borderRadius: '0.5em',
+                }}
+                >
+
+                </Grid2>
+                <Grid2
+                size={6}
+                sx={{
+                    backgroundColor: '#ffffff20',
+                    padding: '1em',
+                    borderRadius: '0.5em',
+                }}
+                >
+                    
                 </Grid2>
             </Grid2>
         </div>
